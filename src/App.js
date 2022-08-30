be86b17030d759675fc44ae2abfcc05f5d1982c0
Emily Mahler <102252888+stradless05@users.noreply.github.com>
2022-08-30 08:11:36 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        Weather App
+        <footer>
+          This project is created by Emily Mahler and is{" "}
+          <a
+            href="https://github.com/stradless05/my-react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
-
-export default App;
